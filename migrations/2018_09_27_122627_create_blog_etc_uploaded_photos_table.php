18b95f5deed53802d5be31c81529c61990b8808e
FirstThinkD <thinkd.developer@gmail.com>
2020-10-16 18:19:17 +0330

    updates readme file

--- migrations/2018_09_27_122627_create_blog_etc_uploaded_photos_table.php
+++ migrations/2018_09_27_122627_create_blog_etc_uploaded_photos_table.php
@@ -1,10 +1,10 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
-class CreateHessamUploadedPhotosTable extends Migration
+class CreateBlogEtcUploadedPhotosTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,16 @@ class CreateHessamUploadedPhotosTable extends Migration
      */
     public function up()
     {
-        Schema::create('hessam_uploaded_photos', function (Blueprint $table) {
+        Schema::create('blog_etc_uploaded_photos', function (Blueprint $table) {
             $table->increments('id');
             $table->text("uploaded_images")->nullable();
             $table->string("image_title")->nullable();
             $table->string("source")->default("unknown");
             $table->unsignedInteger("uploader_id")->nullable()->index();
             $table->timestamps();
+        });
+        Schema::table("blog_etc_posts",function(Blueprint $table) {
+            $table->string("seo_title")->nullable();
         });
     }
 
@@ -30,6 +33,10 @@ class CreateHessamUploadedPhotosTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('hessam_uploaded_photos');
+        Schema::dropIfExists('blog_etc_uploaded_photos');
+
+        Schema::table("blog_etc_posts",function(Blueprint $table) {
+            $table->dropColumn("seo_title");
+        });
     }
 }
